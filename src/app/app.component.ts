07617de3b693280ebeea8547e6ee66c534ec6f39
rsach <rahul.sachdeva@live.com>
2016-12-29 18:55:42 +0530

    git publish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormGroup, FormBuilder, Validators} from "@angular/forms";
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,99 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+    zoom:number = 10;
+    lat:number = 42.858217;
+    lng:number = -70.929990;
+
+  markerForm:FormGroup;
+
+
+   constructor(private fb:FormBuilder){
+      this.markerForm = this.fb.group({
+        name: ['',Validators.required],
+        lat: ['',Validators.required],
+        lng: ['',Validators.required],
+        draggable: [false]
+      });
+  }
+
+
+
+    markers:marker [] =[
+      {
+        name:'Company one',
+        lat: 42.825588,
+        lng: -71.018029,
+        draggable: true
+
+      },
+      {
+        name:'Company two',
+        lat: 42.868164,
+        lng: -71.889071,
+        draggable: true
+
+      },
+      {
+        name:'Company three',
+        lat: 42.858279,
+        lng: -71.930498,
+        draggable: true
+
+      }
+    ];
+
+
+  clickedMarker(marker:marker,i:number){
+    console.log(marker,i)
+
+
+  }
+
+
+  mapClicked($event){
+    var marker = {
+      name: 'untitled',
+      lat: $event.coords.lat,
+      lng: $event.coords.lng,
+      draggable: false
+    }
+
+    this.markers.push(marker);
+
+  }
+
+  markerDragEnd(marker:marker,$event){
+    console.log(marker,$event)
+  }
+
+
+
+  submitForm(){
+    var draggable:boolean = (this.markerForm.value.draggable ==='True')? true:false;
+    var marker = {
+      name: this.markerForm.value.name,
+      lat: this.markerForm.value.lat,
+      lng: this.markerForm.value.lng,
+      draggable: draggable
+
+    }
+
+    console.log(marker);
+
+    this.markers.push(marker);
+  }
+
+
+
+}
+
+
+
+interface  marker{
+  name?:string;
+  lat:number;
+  lng:number;
+  draggable:boolean;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule , ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MapitRoutingModule } from './app-routing.module';
+import { NavbarComponent } from './navbar/navbar.component';
+import {AgmCoreModule} from "angular2-google-maps/core";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ReactiveFormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAnMOqnUA_TdGa3WB-CuUcq-XTD84Y62V4'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
